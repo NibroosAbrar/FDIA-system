8065ed8c9df6e7c54433757ce0d4f91750cec0d9
Muhammad Nibroos Abrar <nibroosabroor@gmail.com>
2025-02-06 20:13:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -303,12 +303,12 @@ dashboard_html = f"""
         body, html {{
             margin: 0;
             padding: 0;
-            width: 100vw;
-            height: 100vh;
+            width: 100%;
+            height: 100%;
         }}
         #superset-container {{
             width: 100vw; 
-            height: 200vh; 
+            height: 100vh; 
             position: relative;
         }}
         iframe {{
