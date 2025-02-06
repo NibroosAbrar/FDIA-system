57df5b99ce97f9b932d90c2e2871586cf4ca911c
Muhammad Nibroos Abrar <nibroosabroor@gmail.com>
2025-02-06 20:09:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -372,7 +372,7 @@ dashboard_html = f"""
                     supersetDomain: supersetUrl,
                     mountPoint: document.getElementById("superset-container"),
                     fetchGuestToken: async () => guest_token,
-                    dashboardUiConfig: {{hideTitle: true, filters.expanded: false }}
+                    dashboardUiConfig: {{hideTitle: true, filters.visible: false }}
                 }});
             }} catch (error) {{
                 console.error("❌ Dashboard error:", error);
